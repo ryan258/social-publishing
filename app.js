711e44b0a39a76a2ca9a82c61d9daf7a8f537104
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-12-04 12:09:42 -0600

    Set up basic routing.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const app = express()
 
-app.get("/", function (req, res) {
-  res.send("Welcome to our new app!")
-})
+const router = require("./router")
+
+// tell express things (name, value)
+app.use(express.static("public"))
+app.set("views", "views")
+app.set("view engine", "ejs")
+
+app.use("/", router)
 
 app.listen(3000)
